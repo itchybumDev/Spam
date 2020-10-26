5ec344032a3bc17ad559fd4e5b2983d16b907b5e
Buffolio <buffolio.bot@gmail.com>
2020-10-26 09:39:49 +0000

    Added

--- pyro.py
+++ pyro.py
@@ -1,24 +1,41 @@
+import datetime
+
 from pyrogram import Client
 
 from messages import message
-
+import time
 
 def main():
-    # api_id = 1936059
-    # api_hash = "3762e62a9653fdb474e1d1ecc9559571"
-    # groupIds = [-431644407, -476219790, -408271358]
-
-    # with Client("my_account", api_id, api_hash) as app:
-    #     for id in groupIds:
-    #         app.send_message(chat_id=id, text="Hello from user")
-
     api_id = 1868620
     api_hash = "7cac9b58cb4d8f833183f51a4866c6d0"
-    print("I am here")
+    print("Starting pyro.py")
+
+    #[[Title, id, frequency, lastSent], [Title, id, frequency, lastSent]]
+    chatGroups = []
+
     with Client("RachelUser", api_id, api_hash) as app:
         for dialog in app.iter_dialogs():
             print(dialog.chat.title or dialog.chat.first_name)
-            print(message)
+            print(dialog.chat.id)
+            # print(message)
+
+        startGroup(app, chatGroups)
+
+
+def startGroup(app, chatGroups):
+    while True:
+        for g in chatGroups:
+            keepSending(app, g)
+
+        time.sleep(30)
+
+
+def keepSending(app, group):
+    #[Title, id, frequency, lastSent]
+    if (isinstance(group[3], int)
+            or (datetime.today() - group[3].lastSent).total_seconds() > group[3].frequency):
+        print("sent to {} - {} -{}".format(group[0], group[1], group[2]))
+        app.send_message(group[2], message, parse_mode="markdown")
 
 
 if __name__ == "__main__":
